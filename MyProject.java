4e0e81f2787044dc83d8997c9d8f6d68a016447c
[DWathanyu <[DWathanyu.boat@mail.kmutt.ac.th>
2022-03-23 12:10:00 +0700

    new file1

--- MyProject.java
+++ MyProject.java
@@ -11,11 +11,9 @@ package myproject;
  */
 public class MyProject {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world!");
     }
     
 }
